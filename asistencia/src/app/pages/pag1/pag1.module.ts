546a657d69adc44d92a8cc5cb81f2d53f879e488
dfredes <101341795+dfredes@users.noreply.github.com>
2022-08-30 21:48:22 -0400

    menu lateral

--- asistencia/src/app/pages/pag1/pag1.module.ts
+++ asistencia/src/app/pages/pag1/pag1.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { Pag1PageRoutingModule } from './pag1-routing.module';
 
 import { Pag1Page } from './pag1.page';
+import { MenuComponent } from 'src/app/components/menu/menu.component';
+
 
 @NgModule({
   imports: [
@@ -15,6 +17,6 @@ import { Pag1Page } from './pag1.page';
     IonicModule,
     Pag1PageRoutingModule
   ],
-  declarations: [Pag1Page]
+  declarations: [Pag1Page, MenuComponent]
 })
 export class Pag1PageModule {}
--- asistencia/src/app/pages/pag1/pag1.page.ts
+++ asistencia/src/app/pages/pag1/pag1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-pag1',
   templateUrl: './pag1.page.html',
